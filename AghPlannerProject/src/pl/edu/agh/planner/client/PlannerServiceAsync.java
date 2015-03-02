cf63dc23ca59493099fdcd310ed4f63657b0ee61
Roger Barlik <roger.barlik@wp.pl>
2015-03-02 19:36:26 +0100

    Sprzatanie smieci 2

--- AghPlannerProject/src/pl/edu/agh/planner/client/PlannerServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.planner.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface PlannerServiceAsync {
-	void getMessage(String msg, AsyncCallback<String> async);
-}
