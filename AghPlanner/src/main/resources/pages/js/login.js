168df7033e8498f3471af60306b6ccb56a711e97
Krystian <krystianujma@gmail.com>
2015-07-15 20:47:12 +0200

    login redirect

--- AghPlanner/src/main/resources/pages/js/login.js
+++ AghPlanner/src/main/resources/pages/js/login.js
@@ -9,8 +9,7 @@ jQuery(document).ready(function ($) {
             url: '/login'
 
         }).done(function(data, textStatus, jqXHR) {
-            var preLoginInfo = JSON.parse($.cookie('dashboard.pre.login.request'));
-            window.location = preLoginInfo.url;
+            window.location='main.html';
 
         }).fail(function(jqXHR, textStatus, errorThrown) {
             alert('Booh! Wrong credentials, try again!');
